67640520440336ab85c47763c4bb7bb461f5ab35
facina <lovemistborn13@gmail.com>
2022-09-13 17:48:34 +0800

    Update 2022-09-13T09:48:32.226Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,44 +1,41 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+"use strict";
 
-// Swiper
-const swiper = new Swiper('.swiper', {
+$(function () {
+  console.log('Hello Bootstrap5');
+}); // Swiper
+
+var swiper = new Swiper('.swiper', {
   // Optional parameters
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
-  
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
+    clickable: true
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+    prevEl: '.swiper-button-prev'
+  }
+}); //swiper-bigger//
 
-//swiper-bigger//
 var swiper2 = new Swiper(".swiper2", {
   slidesPerView: 3,
   spaceBetween: 24,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-//artist-dropdown-btn//
+    clickable: true
+  }
+}); //artist-dropdown-btn//
 
 $(function () {
-	$(".button").click(function (e) {
-		e.preventDefault();
-		$(".dropdownList").slideToggle(500);
-		$(".fa-angle-down").toggleClass("active");
-	});
+  $(".button").click(function (e) {
+    e.preventDefault();
+    $(".dropdownList").slideToggle(500);
+    $(".fa-angle-down").toggleClass("active");
+  });
 });
+//# sourceMappingURL=all.js.map
